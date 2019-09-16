d9f595d1f76450ae383a7474f317e11be7218178
shadow <shadow@LAPTOP-AFD369ML>
2019-09-16 17:05:39 +0800

    4-1装箱和拆箱

--- 第二章继承/Program.cs
+++ 第二章继承/Program.cs
@@ -4,15 +4,15 @@ namespace 第二章继承
 {
     
         interface ICatchMice//定义一个抓老鼠的接口
-    {
+        {
             void CatchMice();
         }
         interface IClimbTree//定义一个爬树的接口
-    {
+        {
             void ClimbTree();
         }
         abstract public class Pet//abstract定义抽象pet基类
-    {
+        {
             public Pet(string petname)
             {
                 Name = petname;
@@ -23,12 +23,12 @@ namespace 第二章继承
                 Console.WriteLine("pet's name is:" + Name);
             }
             abstract public void Speak();//抽象基类,abstract定义抽象speak方法
-    }
+        }
     //1.sealed是定义为密闭类，就是无法在其子类中在进行重写：比如狗类中的叫声基本都是wow，所以当我们派生类定义了一个拉布拉多的狗，则不可以再重写狗叫声的方法。
     /*2.override进行抽象speak方法的重写
      （override可以与virtual虚方法配合用，但是virtual定义的虚方法一般没有多大意义，还是用abstract抽象定义比较好）*/
     class Dog : Pet
-        {
+    {
             static int num;
             static Dog()
             {
@@ -50,7 +50,7 @@ namespace 第二章继承
             {
                 Console.WriteLine(Name + " is speaking:" + "wow");
             }
-        }
+    }
         static class PetGuide//定义一个静态类
         {
             static public void HowToFeedDog(this Dog dog)//静态类里面定义静态方法，this后面的第一个参数是要扩展类的类名，比如扩展狗狗吃东西的方法。
@@ -59,7 +59,7 @@ namespace 第二章继承
             }
         }
         class Cat:Pet,ICatchMice, IClimbTree//注意pet是派生类的名字必须放在第一位，后面接口的位置可以随便乱放（ICatchMice,IClimbTree）
-    {
+        {
             public void CatchMice()
             {
                 Console.WriteLine("猫猫抓到老鼠了！");
@@ -98,6 +98,16 @@ namespace 第二章继承
             Dog.Counts();
             Dog dog = new Dog("DJ");
             dog.HowToFeedDog();
+            {
+                int i = 3;
+                object oi = i;
+                Console.WriteLine(oi.ToString());
+                int j = (int)oi;
+                Console.WriteLine(j);
+
+            }
+
+           
 
             /*Dog dog = new Dog();
             Cat cat = new Cat();
